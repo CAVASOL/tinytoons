a9392992f3e6021fcac3929c23f3b15134460c7e
CAVASOL <awyeon@gmail.com>
2023-12-15 17:42:26 +0900

    test img

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tinytoons/screens/home.dart';
 
-void meh() {
+void main() {
   runApp(const App());
 }
 
-@override
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-        // colorScheme: const ColorScheme(background: Color(0xFFE7626C)),
-      ),
-      home: const Scaffold(
-        body: Center(),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,15 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:tinytoons/models/webtoon_model.dart';
+import 'package:tinytoons/services/api_service.dart';
 
-class HoneScreen extends StatefulWidget {
-  const HoneScreen({super.key});
+class Home extends StatelessWidget {
+  Home({super.key});
 
-  @override
-  State<HoneScreen> createState() => _HoneScreenState();
-}
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
-class _HoneScreenState extends State<HoneScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0.2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        title: const Text(
+          '오늘 웹툰',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 48,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                )
+              ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.black,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        final webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            SizedBox(
+              width: 240,
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {
+                  'Referer': 'https://comic.naver.com',
+                },
+              ),
+            ),
+            Text(webtoon.title),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
+      ),
+    );
   }
 }
